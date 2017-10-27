bd61284f747eea9a961577247898fddf5d5ff6bb
Michael Andino <mikeandinor98@gmail.com>
2017-10-27 14:47:19 -0600

    Metodo Menu

--- Lab3.cpp
+++ Lab3.cpp
@@ -4,15 +4,32 @@ using namespace std;
 
 int main();
 
-int
+void menu();
+
+int ejercicio1();
+
+int ejercicio2();
+
+int ejercicio3();
 
 int*** crearMatriz(int);
 
+void imprimirMatriz(int***,int);
+
 int main(){
-		
+	menu();	
 	return 0;
 }//main
 
+void menu(){
+	cout<<"Programacion 3 Laboratorio 3"<<endl
+		<<" Ejercicio 1: "<<endl
+		<<" Ejercicio 2: "<<endl
+		<<" Ejercicio 3: "<<endl
+		<<" Salir: 4 "<<endl
+		<<" Ingrese su opcion: ";
+	int opcion = 0;
+}
 int*** crearMatriz(int size){
 	int*** matriz = new int**[size];
 	for(int i=0;i<size;i++){
@@ -24,6 +41,8 @@ int*** crearMatriz(int size){
 		}
 	}
 	return matriz
-}//crear matriz
+}//crearMatriz
 
-void imprimirMatriz(inint size)
+void imprimirMatriz(int*** matriz, int size){
+
+}//imprimirMatriz
