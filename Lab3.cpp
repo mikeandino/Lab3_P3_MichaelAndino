7a5787a0d7218dde84ab4ed08aecc9d6c63e27f8
Michael Andino <mikeandinor98@gmail.com>
2017-10-27 15:08:24 -0600

    Menu Hecho

--- Lab3.cpp
+++ Lab3.cpp
@@ -23,15 +23,15 @@ int main(){
 	return 0;
 }//main
 
-void menu(){
+bool menu(){
 	cout<<"Programacion 3 Laboratorio 3"<<endl
-		<<"  Ejercicio 1: Poda y Busca"<<endl
-		<<"  Ejercicio 2: Teorema de Rufinni"<<endl
-		<<"  Ejercicio 3: Vector de Fechas"<<endl
-		<<" Salir: 4 "<<endl
-		<<" Ingrese su opcion: ";
+		<<"  Numero 1: Poda y Busca"<<endl
+		<<"  Numero 2: Teorema de Rufinni"<<endl
+		<<"  Numero 3: Vector de Fechas"<<endl
+		<<"  Numero 4: Salir"<<endl
+		<<"  Ingrese su opcion: ";
 	int opcion = 0;
-	cin<<opcion;
+	cin>>opcion;
 	if(opcion==1){
 
 	}else if(opcion==2){
@@ -39,10 +39,12 @@ void menu(){
 	}else if(opcion==3){
 
 	}else if(opcion==4){
-
+		return false;
 	}else{
 		cout<<"Numero Invalido";
-}
+	}//if y elses
+}//menu
+
 int*** crearMatriz(int size){
 	int*** matriz = new int**[size];
 	for(int i=0;i<size;i++){
@@ -51,9 +53,9 @@ int*** crearMatriz(int size){
 	for(int i=0;i<size;i++){
 		for(int j=0;j<3;i++){
 			matriz[i][j]=new int[size];
-		}
-	}
-	return matriz
+		}//for
+	}//for
+	return matriz;
 }//crearMatriz
 
 void imprimirMatriz(int*** matriz, int size){
